96f5b2f9df084b738311c3bd9e3014e5f2de1946
zhangkai <zhangkai@2345.com>
2022-07-22 09:39:10 +0800

    update: reduce函数等

---
+++ coding/javascript/jsRun.js
@@ -0,0 +1,17 @@
+
+/**
+ * js运行机制
+ * 任务队列：同步、异步（settimeout/dom事件/promise）
+ * eventloop：事件循环
+ * 
+ */
+
+console.log(1);
+setTimeout(() => {
+    console.log(2);
+}, 0);
+console.log(3);
+ for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
+ 
